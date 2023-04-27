9ba8827b0da3286bd4f8a70acd55f5e64296ea8c
Robson <robccorreia@gmail.com>
2023-04-27 17:51:06 -0300

    feat: redux config

---
+++ src/store/configureStore.js
@@ -0,0 +1,12 @@
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
+
+const contador = () => 0;
+
+const reducer = combineReducers({ contador });
+
+const store = configureStore({
+  reducer,
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(),
+});
+
+export default store;
